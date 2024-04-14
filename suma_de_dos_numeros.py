6e64ebc852a02ad2c2c7b46a3d467b289ebdae92
Facundo-Rosales-Martinez <127146276+Facundo-Rosales-Martinez@users.noreply.github.com>
2024-04-14 18:12:15 -0300

    Actualizar agregando archivo .py con el algoritmo

---
+++ suma_de_dos_numeros.py
@@ -0,0 +1,9 @@
+nombre=input("¿Cual es tu nombre? :")
+
+primer_numero=int(input("Hola, " + nombre + " haremos una suma, introduce el primer numero : "))
+
+segundo_numero=int(input("Introduce el segundo numero : "))
+
+resultado= primer_numero + segundo_numero
+
+print("El resultado es ", primer_numero," + ",segundo_numero, " = " ,resultado)
