16807c43b700729454ca40c60b8f035b876bd4d5
Facundo <frm.code@gmail.com>
2024-04-15 14:10:18 -0300

    Formateo correcto de codigo y pequeñas modificaciones de textos

--- suma_de_dos_numeros.py
+++ suma_de_dos_numeros.py
@@ -1,9 +1,11 @@
-nombre=input("¿Cual es tu nombre? :")
+nombre = input("¿Cual es tu nombre? :")
 
-primer_numero=int(input("Hola, " + nombre + " haremos una suma, introduce el primer numero : "))
+primer_numero = int(
+    input("Hola, " + nombre + " haremos una suma, introduce el primer numero : "))
 
-segundo_numero=int(input("Introduce el segundo numero : "))
+segundo_numero = int(input("Introduce el segundo numero : "))
 
-resultado= primer_numero + segundo_numero
+resultado = primer_numero + segundo_numero
 
-print("El resultado es ", primer_numero," + ",segundo_numero, " = " ,resultado)
+print("El resultado de ", primer_numero,
+      " + ", segundo_numero, " es = ", resultado)
